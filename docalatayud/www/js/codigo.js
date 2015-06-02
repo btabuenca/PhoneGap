ae741dbf3a74264c230d8d6d73f19b8114b91291
btabuenca <bernardo.tabuenca@gmail.com>
2015-06-02 11:21:05 +0200

    PhoneGap apps

---
+++ docalatayud/www/js/codigo.js
@@ -0,0 +1,17 @@
+﻿/*función CUANDO LA PÁGINA SE CARGUE
+$(document).ready(function() {
+	
+	
+	$("#rutapiedra").click(function(){
+			map = new GMaps({
+			  div: '#map_piedra',
+			  lat: 41.276258,
+			  lng: -1.840897
+			});	
+
+					
+					
+	});
+
+});
+*/
